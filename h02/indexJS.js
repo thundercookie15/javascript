b1293a9f5e82b00468612ca614034714de0e34ae
lucas-maijers <lucas.maijers@student.rocvf.nl>
2019-10-09 11:03:50 +0200

    Opdracht 1

---
+++ h02/indexJS.js
@@ -0,0 +1,5 @@
+document.getElementById("paginatitel").innerHTML = "Dit is de paginatitel";
+document.getElementById("subkop").innerHTML = "Dit is de subkop";
+
+document.getElementById("aap1").src = "images/aap1.jpg";
+document.getElementById("aap2").src = "images/aap2.jpg";
---
+++ h02/stijl.js
@@ -0,0 +1,24 @@
+// Kleuren
+
+document.getElementById("bl1").style.backgroundColor = "aqua";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+
+// Locatie
+document.getElementById("bl2").style.marginTop = "20px";
+document.getElementById("bl3").style.marginTop = "40px";
+
+// Locatie Tekst
+/* Blokje 1 */
+document.getElementById("bl1").style.paddingTop = "15px";
+document.getElementById("bl1").style.textAlign = "center";
+
+/* Blokje 2 */
+document.getElementById("bl2").style.textAlign = "center";
+
+/* Blokje 3 */
+document.getElementById("bl3").style.textAlign = "right";
+
+/* Blokje 4 */
+document.getElementById("bl4").style.textAlign= "left";
